12c8fc2ab834e2c4efb9aa1122d66c2e947b28a6
ethnwr <daniil.gawrilov07@gmail.com>
2025-05-02 23:09:39 +0900

    Create ConsoleApplication.cpp

---
+++ ConsoleApplication.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+#include "Elevator.h"
+
+using namespace std;
+
+
+int main() {
+    setlocale(LC_ALL, "ru");
+    Elevator elevator(1, 10); // Создаем лифт с диапазоном от 1 до 10 этажей
+
+    elevator.turnOn(); // Включаем лифт
+
+    elevator.callElevator(5); // Вызываем лифт на 5 этаж
+    elevator.callElevator(11); // Попытка вызвать лифт на недопустимый этаж
+
+    cout << "Текущий этаж: " << elevator.getCurrentFloor() << endl; // Получаем текущий этаж
+
+    elevator.turnOff(); // Выключаем лифт
+
+    return 0;
+}
